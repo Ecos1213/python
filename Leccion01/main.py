203e89538cbce1f86d1c3c2748bc60351f8ef1dd
Ecos1213 <diasflac1213@gmail.com>
2024-04-30 21:09:53 -0500

    Agrega leccion de variables en python donde se agrega nuevas variables imprime, hace sumatoria e imprime su valor

--- Leccion01/main.py
+++ Leccion01/main.py
@@ -1,2 +1,18 @@
-# Enviar un saludo a la consola utilizando Python
-print('Este es mi saludo desde Python')
+miVariable = "Hola Mundo desde Python"  #esto es un string, en python no tenemos que indicar el tipo de dato de nuestra variable
+miVariable = 2
+x = 10
+y = 2
+z = x + y
+
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+miVariable = 10
+print(miVariable)
+
+print(x)
+print(y)
+print(z)
+
+
