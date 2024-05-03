611cb39e71fb8bfcb2362a350d67df00bbb979c2
Ecos1213 <diasflac1213@gmail.com>
2024-05-03 11:16:05 -0500

    Se cambia las variables para conocer los tipos de datos de python donde hay una imagen explicativa de los tipos de datos que hay en python y el codigo se muestra los datos tipo string, integer, float, boolean y se ve el indicador hint que son : que sirve para indicar que la variable va a ser de tipo str o int o etc, esto es mas indicativo por que no genera error si se coloca otro tipo que no sea el de el indicatido de la variable otra cosa a tener en cuenta es el uso del metodo type que es para ver que tipo de dato de la variable

--- Leccion01/main.py
+++ Leccion01/main.py
@@ -1,8 +1,21 @@
-#Declaracion de variables
-nombre = 'Juan Perez'  #se puede usar comilla simple o comilla doble para un string
-telefono = 55618867
-email = "jperez@gmail.com"
+x = 10  #Tipo numeric y es un integer
+print(x)
+print(type(x))  #el metodo type lo que hace es ver que tipo de dato es el valor de la variable
 
-print(nombre) #recuerda hay que respetar la nomenclatura de varialbes en python, esto incluye que las variables son diferentes si colocamos simbolos, minusculas o mayusculas(python es sensible a mayusculas y minusculas) o numeros
-print(telefono)
-print(email)
+x = "Hola mundo"  #Tipo sequence type y es un string
+print(x)
+print(type(x))
+
+x: str = 'Hola mundo'  #Tipo sequence type y es un string, en python podemos apuntar al tipo de dato de una variable esto lo podemos hacer con algo llamado hint que es una pista o indicacion para decirle que es de un tipo de dato, hay que usar : despues de la variable y colocar el tipo de dato que queremos indicarle, el tipo de dato tiene que escribirse de forma corta osea str, int, etc. si colocamos un valor no correspondiente al valor del tipo de dato de la pista no pasara alguin error pero el ide nos recordara resaltando que no es el tipo de dato que esta definido
+print(x)
+print(type(x))
+
+x = 10.5  #Tipo numeric y es un float
+print(x)
+print(type(x))
+
+x = True  #Tipo boolean y es un boolean, recuerda no se puede escribir true ni false tiene escribirse la primera en mayuscula si no no lo reconocera True y False
+print(x)
+print(type(x))
+
+#python es un lenguaje de programacion orientado a objetos por lo tanto los tipos de datos tambien son alamacenados en clases
