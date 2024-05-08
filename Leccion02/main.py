f0aafac7e44283337cd0d72265e1938368f6576f
Ecos1213 <diasflac1213@gmail.com>
2024-05-08 16:14:45 -0500

    Agrega leccion de operadores logicos como and or y not, el and or son operadores binarios por que se necesitan de dos operadores para poder ejecutarse y el not es unario por que necesita de un solo operador para poder ejecutarse

--- Leccion02/main.py
+++ Leccion02/main.py
@@ -1,27 +1,28 @@
-#Operadores de operacion, nos sirve para comparar si dos valores son distintos o iguales
-a = 4
-b = 2
+#Operadores logicos en python
+"""
+los operadores logicos son:
+operador               descripcion                                            uso
+and                    devuelve True si ambos operadores son true             a and b
+or                     devuelve True si alguno de los operadores es True      a or b
+not                    devuelve True si alguno de los operandos False         not a
+                       esto quiere decir que si el valor es True
+                       cambia a False o si es False cambia a True
 
-# en este caso por presedencia de operadores primero se evalua lo que esta la derecha y luego lo que esta en la izquierda osea que primero hara la igualdad y despues asignara el resultado
-resultado = a == b  # a es igual a b?; este es el simbolo de igualdad en python ==
-print(f'Resultado == : {resultado}')
+en el uso puede ser una expresion o un valor booleano
+y el operador not es unario osea que necesita de un operador para funcionar o el resultado de una expresion o el valor de a
 
-# la presedencia en este caso sucede lo mismo que con el caso anterior
-resultado = a != b  # a es diferente a b?; este es el simbodo de diferente en python !=
-print(f'Resultado != : {resultado}')
+"""
 
-# la presedencia en este caso sucede lo mismo que con el caso anterior
-resultado = a > b  # a es mayor a b?; este es el simbodo de mayor en python >
-print(f'Resultado > : {resultado}')
+a = True
+b = False
+resultado = a and b  # esto es false
+print(resultado)
 
-# la presedencia en este caso sucede lo mismo que con el caso anterior
-resultado = a >= b  # a es mayor o igual a b?; este es el simbodo de mayor e igual en python >=
-print(f'Resultado >= : {resultado}')
+resultado = a or b  # esto es true
+print(resultado)
 
-# la presedencia en este caso sucede lo mismo que con el caso anterior
-resultado = a < b  # a es menor a b?; este es el simbodo de menor en python <
-print(f'Resultado < : {resultado}')
+resultado = not a  # esto es false
+print(resultado)
 
-# la presedencia en este caso sucede lo mismo que con el caso anterior
-resultado = a <= b  # a es menor o igual a b?; este es el simbodo de menor e igual en python <=
-print(f'Resultado <= : {resultado}')
+# el operador and y or son operadores binarios ya que se necesitan de dos valores para poder ejecutarse
+# el operador not es unario ya que se necesita solo de un valor para poder ejecutarse
