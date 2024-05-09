771e690fdd7902c0070e03a94dab592921f9aa2c
Ecos1213 <diasflac1213@gmail.com>
2024-05-09 18:57:01 -0500

    Agrega leccion usdando el operador logico not, el simbolo not se usa igual en python y es similar al simbolo ! de php y javascript que lo que hace es denegar de true a false o de false a true, este se usa igual que el !, que es usandolo antes de una variable o de un resultado de una variable que este encerrado en parentesis

--- Leccion02/main.py
+++ Leccion02/main.py
@@ -1,7 +1,8 @@
 vacaciones = True
 diaDescanso = False
 
-if vacaciones or diaDescanso:
-    print('Puede asistir al juego')
-else:
+#if not vacaciones or not diaDescanso: #asi como en php o javascript podemos denegar el true por false o el false por true en php y javascript se usa ! pero en este caso se usa el not antes de la variable, pero como estamos denegando las dos podemos encerrar con un parentesis para que cuando de el resultado deniege el resultado
+if not (vacaciones or diaDescanso):
     print('Tiene deberes por hacer')
+else:
+    print('Puede asistir al juego')
