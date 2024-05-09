9c236c0bf3d9f020cf5d9782ffff4a20c0977f81
Ecos1213 <diasflac1213@gmail.com>
2024-05-09 16:20:34 -0500

    Agrega leccion de usar el oiperador logico or y se agrega una imagen de diagrama de flujo del problema que es con or verificar si tiene la persona vacaciones o dias de descanso para ver el juego

--- Leccion02/main.py
+++ Leccion02/main.py
@@ -1,13 +1,7 @@
-valor = int(input('Escribe el valor: '))
-valorMinimo = 0
-valorMaximo = 5
+vacaciones = True
+diaDescanso = False
 
-#dentroRango = (valor >= valorMinimo) and (valor <= valorMaximo) #los parentesis son opcionales asi que comentamos esta linea y quitamos los parentesis
-dentroRango = valor >= valorMinimo and valor <= valorMaximo
-
-if dentroRango:
-    print(f'El valor {valor} está dentro de rango')
+if vacaciones or diaDescanso:
+    print('Puede asistir al juego')
 else:
-    print(f'El valor {valor} está fuera de rango')
-
-
+    print('Tiene deberes por hacer')
