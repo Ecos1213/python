de5aa68f8d1033baff1de6b46cf54928c138888b
Ecos1213 <diasflac1213@gmail.com>
2024-05-09 14:23:19 -0500

    Ejercicio donde el usuario tiene que digitar un numero y con un if y el operador logico and vemos si esta dentro del rango 0 a 5

--- Leccion02/main.py
+++ Leccion02/main.py
@@ -1,28 +1,13 @@
-#Operadores logicos en python
-"""
-los operadores logicos son:
-operador               descripcion                                            uso
-and                    devuelve True si ambos operadores son true             a and b
-or                     devuelve True si alguno de los operadores es True      a or b
-not                    devuelve True si alguno de los operandos False         not a
-                       esto quiere decir que si el valor es True
-                       cambia a False o si es False cambia a True
+valor = int(input('Escribe el valor: '))
+valorMinimo = 0
+valorMaximo = 5
 
-en el uso puede ser una expresion o un valor booleano
-y el operador not es unario osea que necesita de un operador para funcionar o el resultado de una expresion o el valor de a
+#dentroRango = (valor >= valorMinimo) and (valor <= valorMaximo) #los parentesis son opcionales asi que comentamos esta linea y quitamos los parentesis
+dentroRango = valor >= valorMinimo and valor <= valorMaximo
 
-"""
+if dentroRango:
+    print(f'El valor {valor} está dentro de rango')
+else:
+    print(f'El valor {valor} está fuera de rango')
 
-a = True
-b = False
-resultado = a and b  # esto es false
-print(resultado)
 
-resultado = a or b  # esto es true
-print(resultado)
-
-resultado = not a  # esto es false
-print(resultado)
-
-# el operador and y or son operadores binarios ya que se necesitan de dos valores para poder ejecutarse
-# el operador not es unario ya que se necesita solo de un valor para poder ejecutarse
