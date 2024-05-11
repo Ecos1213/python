d082041046ace1abf9ac211f14258d8e5aac5217
Ecos1213 <diasflac1213@gmail.com>
2024-05-10 22:06:12 -0500

    Agrega leccion donde simplificamos el if anterior el cual cambiamos esto: (edad >= 20 and edad < 30) or (edad >= 30 and edad < 40) a esto (20 <= edad < 30) or (30 <= edad < 40) y es exactamente lo mismo ya que en python podemos simplicifar mas la condicional del if, que es simplificando y cambiando los operadores logicos, simplemente quitamos las dos variables de edad y la dejamos en el centro y dejamos el operador logico de la derecha igual el que tiene que cambiar e invertirese es de la izquierda ya que como vemos la primera expresion es >= pero como ahora pasa al lado derecho del numero entonces hau que invertir el > por < ya que estamos preguntando si edad es mayor que el numero, esto se lee igual osea edad es mayor o igual que 20 y edad es menor a 30 o 30 es menor o igual que edad y es menor que 40

--- Leccion02/main.py
+++ Leccion02/main.py
@@ -1,20 +1,9 @@
 edad = int(input('Introduce tu edad: '))
-
-#veintes = edad >= 20 and edad < 30
-#print(veintes)
-#treintas = edad >= 30 and edad < 40
-#print(treintas)
-#no es comun que nuestro codigo este separado de los operadores logicos con las condicionales por eso se comento y se colocara en las condicionales, pero esto hace perder la parte del if anidado que es el if de veintes y el elif treintas y el else que es el que le sigue
-
-#if veintes or treintas:
-if (edad >= 20 and edad < 30) or (edad >= 30 and edad < 40):
-    print('Dentro de rango (20\') o (30\'s)') # como vemos necesitamos colocar comilla simple dentro de la cadena, hay dos formar para que nos imprima dentro de la cadena una es usando \ (a esto se le llama caracter de escape ya que le dice al leguanje que no debe cerrar la cadena osea que no considere este caracter como el cierre de nuestra cadena) antes de la comilla simple que queremos imprimir o la otra es usando comilla dobles para iniciar la cadena y terminar la cadena con comillas dobles
-#    if veintes:  # como estas condicionales esta dentro del if por su identacion, hace que este dentro del if, este if interno se le conoce un if anidado, un if dentro de un if
-#        print('Dentro de los 20\'s')
-#    elif treintas:  #como vemos en python no se escribe else if si no elif y hace lo mismo que un else if ya que es lo mismo
-#        print('Dentro de los 30\'s')
-#    else:
-#        print('Fuera de rango')
+#edad >= 20 and edad < 30 # esto es lo mismo que el primer conjunto de operador logico de and
+#edad >= 30 and edad < 40 # esto es lo mismo que el segundo conjunto de operador logico de and
+if (20 <= edad < 30) or (30 <= edad < 40):  # en python podemos simplicifar mas la condicional del if, que es simplificando y cambiando los operadores logicos, simplemente quitamos las dos variables de edad y la dejamos en el centro y dejamos el operador logico de la derecha igual el que tiene que cambiar e invertirese es de la izquierda ya que como vemos la primera expresion es >= pero como ahora pasa al lado derecho del numero entonces hau que invertir el > por < ya que estamos preguntando si edad es mayor que el numero
+    #esto se lee igual osea edad es mayor o igual que 20 y edad es menor a 30 o 30 es menor o igual que edad y es menor que 40
+    print('Dentro de rango (20\') o (30\'s)')
 else:
-    print("No esta dentro de los 20's ni 30's") # como vemos usamos la segunda opcion que es usar comilla doble para la cadena y poder imprimir las comillas simples
+    print("No esta dentro de los 20's ni 30's")
 
