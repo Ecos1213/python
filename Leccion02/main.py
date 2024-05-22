853b79b93e1c37f5ead26c5f231e90371738abf4
Ecos1213 <diasflac1213@gmail.com>
2024-05-21 20:30:25 -0500

    Agrega el ejercicio de tienda de libros donde usamos la conversion de cada tipo de dato que viene del metodo input, pero se encuentra que el metodo bool para convertir de string a booleano lo que hace es que si viene un character esto sera True y si viene vacio esto sera False por eso usamos un if elif y else que determine si el texto es True agregamos la variable con el booleano True si no es un True entonces si viene un False entonces se asigne el booleano False y si no mandar un mensaje, tambien se ve que se puede usar un print con multi linea usando la triple comilla simple ''' ''' esto lo usamos dentro del print e incluso podemos colocar el f para la interpolacion de datos esta triple comilla simple tomara tanto saltos de linea, tabulacion etc ya que formatea bien el texto

--- Leccion02/main.py
+++ Leccion02/main.py
@@ -1,19 +1,28 @@
-"""
-  instrucciones de tarea:
-  solicitar al usuario dos valores y determinar cual numero es el mayor
-  solicitar al usuario dos valores:
-  numero1 (int)
-  numero2 (int)
-  se debe imprimir el mayor de los dos numeros (la salida debe ser identica a la que sigue):
-  Proporciona el numero1:
-  Proprociona el numero2:
-  El numero mayor es: <numeroMayor>
-"""
+print("Proporcione los siguientes datos del libro:")
+nombre = input("Proporciona el nombre: ")
+codigo = int(input ("Proporciona el ID: "))
+precio = float(input("Proporcine el precio: "))
+#envioGratis = bool(input("Indica si el envio es gratuito (True/False): ")) # en python no podemos usar el metodo bool por que los valores que lleguen como cadena osea cualquier character que llegue lo convertira como true y si es un valor vacio lo convertira como false
+envioGratis = input("Indica si el envio es gratuito (True/False): ")
 
-numero1 = int(input("Proporciona el Numero 1: "))
-numero2 = int(input("Proporciona el Numero 2: "))
-
-if numero1 > numero2:
-    print(f'El numero {numero1} es mayor')
+#esta es una manera en que nos que llegue como True en cadena pasarlo a booleano, por que el bool siempre tomara true por cualquier caracter y si esta vacio sera false
+if envioGratis == 'True':
+    envioGratis = True
+elif envioGratis == 'False':
+    envioGratis = False
 else:
-    print(f'El numero {numero2} es mayor')
+    envioGratis = 'Valor incorrecto, debe escribir True/False'
+
+#print(f'Nombre: {nombre}')
+#print(f'Codigo: {codigo}')
+#print(f'Precio: {precio}')
+#print(f'Envio Gratuito?: {envioGratis}')
+
+# en python si en el print queremos agregar inforamcion en varias lineas dentro del mismo print podemos usar triple comillas simples y asi tomara los tabuladores e incluso los saltos de linea, es una cadena ya con formato como se ve en el siguiente codigo, incluso podemos usar f para interpolacion de datos
+print(f'''
+    Nombre: {nombre}
+    Id: {codigo}
+    Precio: {precio}
+    Envio Gratiuto? {envioGratis}
+''')
+
