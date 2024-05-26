207c0ada355571ffec17db82c18e994c1ccea7a3
Ecos1213 <diasflac1213@gmail.com>
2024-05-26 11:55:26 -0500

    Agrega leccion de operador terniario, a diferencia de otros lenguajes el operador terniario de python primero se debe colocar lo que se ejecutara si es verdadero y despues el if con el condicional despues el else y al lado derecho del else se coloca lo que se va a ejecutar si es false

--- Leccion03/main.py
+++ Leccion03/main.py
@@ -1,16 +1,13 @@
-numero = int(input('Proporciona un valro entre 1 y 3: '))
+condicion = True
 
-numeroTexto = ''
+# if condicion:
+#    print('Condicion verdadera')
+# else:
+#    print('Condicion falsa')
 
-if numero == 1:
-    #recuerda que si declaramos variable solo dentro del if recuerda el scope esa variable solo funcionara en elif por eso es mejor declarar fuera la variable por seguridad de que no sucede un error por culpa del scope por eso numerotexto la declaramos afuera
-    numeroTexto = 'Numero uno'
-elif numero == 2:
-    numeroTexto = 'Numero dos'
-elif numero == 3:
-    numeroTexto = 'Numero tres'
-else:
-    numeroTexto = 'Valor fuera de rango'
+#sintaxis simplificada operador ternario
+#el operador ternario es un poco diferente en los otros lenguajes primero tenemos que colocar lo que se ejecutara si la condicion es true despues la condicion con el if y el else al final y a su lado derecho lo que se ejecutara si la condicion es falsa o mejor dicho lo que ejecutara cuando entre al else
+print('Condicion verdadera') if condicion else print('Condicion falsa')
 
-print(f'Numero proporcionado: {numero} - {numeroTexto}')
-
+# recuerda el operador terniario se utiliza cuando tenemos pocas lineas de codigo esto quiere decir que si el if tiene solo una linea de codigo y el else se recomienda usar el operador terneario pero si tiene mas de una linea de codigo se recomienda usar la condicional normal ya que no se podra ejecutar estas lineas de codigo
+# ademas que el operador terniario no usa elif (else if)
