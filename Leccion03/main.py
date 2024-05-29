3705021096fa0a751f6e0e34ba19342bd3108ea6
Ecos1213 <diasflac1213@gmail.com>
2024-05-28 19:00:01 -0500

    Ejercicio de etapas de la vida con un if verficamos rangos de edad y segun esos rangos mandamos un mensaje, tambien usamos el if donde solo tiene una variable que vimos anteriormente

--- Leccion03/main.py
+++ Leccion03/main.py
@@ -1,19 +1,13 @@
-mes = int(input('Proporciona mes del año (1-12): '))
-estacion = None # en python podemos declarar una variable como None, esto sirve para indicarle que esta variable no va a contener ningun valor
+edad = int(input('Proporciona tu edad: '))
+mensaje = None
 
-# esto tiene como beneficion si hacemos la siguiente condicional, verificando si la variable contiene none ejecute algo
-# if estacion == None
-
-if mes == 1 or mes == 2 or mes == 12:
-    estacion = 'Invierno'
-elif mes == 3 or mes == 4 or mes == 5:
-    estacion = 'Primavera'
-elif mes == 6 or mes == 7 or mes == 8:
-    estacion = 'Verano'
-elif mes == 9 or mes == 10 or mes == 11:
-    estacion = 'Otoño'
+if 0 < edad < 10:
+    mensaje = 'La infancia es increible....'
+elif 10 <= edad < 20:
+    mensaje = 'Muchos cambios y mucho estudio....'
+elif 20 <= edad < 30:
+    mensaje = 'Amor y comienza el trabajo....'
 else:
-    estacion = 'Mes incorrecto'
+    mensaje = 'etapa de la vida no reconocida...'
 
-print(f'Para el mes {mes} la estación es: {estacion}')
-
+print(f'tu eddad es: {edad}, {mensaje}')
