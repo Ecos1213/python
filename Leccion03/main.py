9499fe407d0d534d4572559270177e1939b54c31
Ecos1213 <diasflac1213@gmail.com>
2024-05-30 16:43:42 -0500

    Agrega ejercicio de calificacion, el usuario proporciona un valor de la calificacion y segun el valor se determinara con rangos en condicionales, usando el simplify chained, y segun ese rango imprimira el calificacion de A o B o C o D o F o Valor incorrecto, recuerda que el simplify chained es el if simplificado de la siguient forma if 9 <= calificacion <= 10: imprimara algo, ademas agrega una imagen del diagrama de flujo

--- Leccion03/main.py
+++ Leccion03/main.py
@@ -1,13 +1,32 @@
-edad = int(input('Proporciona tu edad: '))
+"""
+Instrucciones de la tarea:
+El objetivo del ejercicio es crear un sistema de calificaciones, como sigue:
+El usuario proporcionara un valor entre 0 y 10
+Si esta entre 9 y 10: imprimira una A
+Si esta entre 8 y menor a 9: imprimira una B
+Si esta entre 7 y menor a 8: imprimira una C
+Si esta entre 6 y menor a 7: imprimira una D
+Si esta entre 0 y menor a 6: imprimira una F
+cualquier otor valor debe imprimir: valo incorrecto
+por ejemplo:
+Proporciona un valor entre 0 y 10:
+A
+"""
+
+calificacion = int(input('Proporciona una calificacion entre 0 y 10: '))
 mensaje = None
-
-if 0 < edad < 10:
-    mensaje = 'La infancia es increible....'
-elif 10 <= edad < 20:
-    mensaje = 'Muchos cambios y mucho estudio....'
-elif 20 <= edad < 30:
-    mensaje = 'Amor y comienza el trabajo....'
+#si esta entre 9 y 10, imprimir A
+if 9 <= calificacion <= 10: # esto es lo mismo que calificacion >= 9 and calificacion <= 10 y recuerda que esto solo funciona con and
+    mensaje = 'La calificacion es ' + str(calificacion) + ' y tienes una A'
+elif 8 <= calificacion < 9:
+    mensaje = 'La calificacion es ' + str(calificacion) + ' y tienes una B'
+elif 7 <= calificacion < 8:
+    mensaje = 'La calificacion es ' + str(calificacion) + ' y tienes una C'
+elif 6 <= calificacion < 7:
+    mensaje = 'La calificacion es ' + str(calificacion) + ' y tienes una D'
+elif 0 <= calificacion < 6:
+    mensaje = 'La calificacion es ' + str(calificacion) + ' y tienes una F'
 else:
-    mensaje = 'etapa de la vida no reconocida...'
+    mensaje = 'Valor incorrecto'
 
-print(f'tu eddad es: {edad}, {mensaje}')
+print(mensaje)
