92f8dff6f197056aaa62de020966eb3a0aa2630d
Ecos1213 <diasflac1213@gmail.com>
2024-05-31 15:50:48 -0500

    Agrega ejercicio de ciclo while el cual imprime los numeros de 0 a 5 y aumenta un contador

--- leccion04/main.py
+++ leccion04/main.py
@@ -1,15 +1,8 @@
-# condicion = True
-#
-# while condicion: # asi se escribe un while en python, ejecutara el ciclo hasta que la condicion sea false
-#     print('Ejecutando ciclo while')
-# else: #en while tambien podemos colocar un else en python, este se ejecutara cuando la condicion del while no se cumpla y tambien es opcional colocar el else
-#     print('fin del ciclo while')
+# Imprimir los numeros enteros del 0 al 5 utilizando un ciclo while
 
+maximo = 5
 contador = 0
-while contador < 3:
-     print(contador)
-     # en python no se puede hacer los operadores de incremento que son ++ o -- por ende tiene que usarse +=
-     contador += 1 # contador = contador + 1
-else:
-    # en el else se puede imprimir cualquier otro codigo, si el while no cumple la condicion se ejecutara este else
-    print('Fin ciclo while')
+while contador <= maximo:
+    print(contador)
+    # contador = contador + 1
+    contador += 1
