4687ad211c0cb98abd7cccc9310aea252799da67
Ecos1213 <diasflac1213@gmail.com>
2024-06-01 10:53:25 -0500

    Agrega ciclo for en python, usamos realmente el ciclo foreach el cual se hace con el ciclo for y es de esta manera for letra in cadena, letra tendra cada valor del array cadena y esto se debe usar con arrays, tambien agrega el else en el ciclo for asi como el while pero la diferencia es que cuando termine el ciclo for ejecutara el codigo else

--- leccion04/main.py
+++ leccion04/main.py
@@ -1,7 +1,9 @@
-# imprimir numeros de 5 a 1 de manera descendente
-minimo = 1
-contador = 5
+cadena = 'Hola' # recuerda una cadena es un arreglo de caracteres y cada caracter es un indice 0 es h, 1 es o, 2 es l, 3 es a
 
-while contador >= minimo:
-    print(contador)
-    contador -= 1 # contador = contador - 1
+# en este caso esto seria el ciclo foreach, ya que va a iterar cada uno de los elementos
+for letra in cadena: # como vemos asi se hace un for each en python y es con el for
+    print(letra) # la primera variable es cada valor del array, por eso usamos la variable letra
+else: # opcionalmente podemos colocar un else asi como el while la diferencia es que cuando termine de recorrer el ciclo for o foreach ejecutara el codigo del else
+    print('Fin ciclo for')
+
+
