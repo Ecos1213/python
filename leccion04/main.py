b3557288b5bcbc9055da7f37f6c03afda9040969
Ecos1213 <diasflac1213@gmail.com>
2024-06-02 12:16:45 -0500

    Agrega leccion de continue, en los ciclos podemos usar la palabra reservada continue para saltarnos las siguientes lineas de codigo y continuar con la siguiente iteracion del ciclo ademas vimos que en python no existe la forma de declarar el for de la siguiente manera for(i=0; i<=6; i++) de hecho en python hay otra manera de hacer este tipo de for y es de la siguiente manera for i in range(6) lo que hace es que el metodo range crea un array segun el parametro que le pasemos en este caso seria un array de 0 hasta 6 y el for lo recorre ya que este for realmente es un foreach y cada valor lo coloca en la letra i y si quieremos que vaya de 1 a 6 el metodo de range podemos pasarle otro parametro range(1,6) y ahi si haria un array de 1 hasta 6

--- leccion04/main.py
+++ leccion04/main.py
@@ -1,6 +1,10 @@
-for letra in 'Holanda': # en el ciclo for del foreach podemos colocar toda la cadena en el foreach como se ve aqui
-    if letra == 'a': # usamos el if para verificar si existe una letra a y si es asi imprimeme una letra y despues con el break interrumpimos el ciclo permanentemente y por esto el else no se ejecuta
-        print(f'Letra encontrada: {letra}')
-        break # el break rompe el ciclo por lo tanto apenas encuentre una 'a' rompera el ciclo y no continuara y el else tampoco se ejecutara por que al romper el ciclo el else del ciclo no se ejecuta
-else:
-    print('Fin ciclo for')
+# en python no se puede hacer ciclos for de esta manera: for i = 1; i < 5; i++ ya que tiene una manera diferente de hacerlo y es usando un foreach que vaya de 0 a 5 y se hace de la siguiente manera
+# for i in range(6): # como vemos el metodo range lo que hace es un array de numeros de un rango estipulado en su parametro podemos colocar solo un parametro de numero que esto significa que creara un array de 0 al numero que coloquemos como parametro en este caso seria hasta 6 pero incluso podemos hacer que sea de 1, 5 que seria que empezaria de 1 hasta 5 osea crearia un array de (1,2,3,4,5) y ya el ciclo for (foreach de python) se encargaria de recorrer cada uno y lo colocara en la letra i cada uno
+#     if i % 2 == 0: # numeros pares
+#         print(f'Valor: {i}')
+
+# como vimos lo comentado anteriormente usamos un for que recorra de 0 a 6 y imprima los numeros paras esto lo podemos hacer con el shortcut continue
+for i in range(6):
+    if i % 2 != 0: # if para saber si es un numero impar
+        continue  # lo que hace continue es no ejecutar las siguientes lineas de codigo y salta a la siguiente iteracion del ciclo
+    print(f'Valor: {i}')
