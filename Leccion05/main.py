7ca5ebaec5be6f75d105c0e2d667e370f6b50c53
Ecos1213 <diasflac1213@gmail.com>
2024-06-11 17:06:14 -0500

    Agrega leccion de diccionarios, los diccionarios son una coleccion de datos que llevan una llave y esta llave contiene un valor, es parecido a un set ya que los diccionarios no tienen indices como los set pero si llevan un orden de ingreso, para definir un diccionario usamos las llaves {} y dentro lleva una 'key':'valor' tanto la llave como la key pueden ser valores de cualquier tipo, las keys se pueden usar cuarlquier tipo inmutable (int, float, bool, str, etc), ademas que podemos modificar, eliminar y agregar nuevos valores, para acceder un valor especifico tenemos que usar la llave que contiene el valor que queremos obtener y se usa de esta manera diccionario['key'] tambien podemos usar el metodo len y como argumento el diccionario para que nos muestre la cantidad de datos que contiene (recuerda que tomara key y elemento como uno solo), tambien podemos acceder al valor del key usando el metodo anidado get y como argumento tenemos que pasarle el key al valor que queremos acceder, para modificar un valor podemos hacer lo siguiente diccionar['key'] = 'valor' y asi modificara el valor, esta es la parte 1 del comentario despues seguira la parte 2

--- Leccion05/main.py
+++ Leccion05/main.py
@@ -1,49 +1,25 @@
-# coleccion de tipo set, a cambio de la tupla y las listas es que el set no permite un orden y no permite elementos duplicados
-# en el set no permite modificar los elementos almacenados, pero es posible agregar mas elementos o eliminarlos
-# en los set al no tener un orden cuando mandamos a imprimir estos elementos el orden es completamente aleatorio ya que no tenemos un indice con el cual apoyarnos y/o ordenar estos elementos, asi que por lo tanto un set no mantiene un orden, ademas que no es posible modificar estos elementos una vez que agregamos el elemento al set pero podemos agregar nuevos elementos y eliminarlos, recuerda el set no permite elementos duplicados
+# los diccionarios son colecciones de datos organizados, a diferencia de los otros eston llevan una llave y esa llave tiene un valor, tal cual como se maneja los diccionarios en la vida real
 
-# set
-planetas = {'Marte', 'Jupiter', 'Venus'} # como vemos para definir un set necesitamos definirlo con llaves {}
-print(planetas) # como vemos cada vez que ejecutamos el orden es aleatorio
+#dict, los diccionarios tambien usan llaves como los set, pero recuerda a diferencia este tendra una llave y despues el dato, la llave se pueden usar cuarlquier tipo inmutable (int, float, bool, str, etc)
+diccionario = {
+    'IDE': 'Integrated Development Environment',
+    'OOP': 'Object oriented programming',
+    'DBMS': 'Database Management System'
+}
 
-#el set tiene tambien metodos similares con una tupla y una lista
+print(diccionario) # como vemos primero imprimira la llave y despues el dato y asi sucesivamente por cada valor
 
-# len para largo de los elementos (osea la cantidad de elementos que tiene en este caso el set)
-print(len(planetas))
+# como los set, tuplas y listas podemos usar el metodo len y despues el argumento que en este caso seria nuestro diccionario de datos para saber la cantidad de elementos que tiene
+print(len(diccionario)) # como vemos nos retornara que contiene 3 valores ya que cuenta es cada valor y llaves juntas osea que son 3 en total
 
-# en un set podemos ver si un elemento ya existe
-# revisar si un elemento esta presente
-# con 'Marte' in planetas verifica si Marte existe en el set de planetas, si existe nos retornara True si no False
-print('Marte' in planetas)
+# un diccionario se parece un poco a un set por que los diccionarios no tienen indices pero si tienen un orden osea que asi como lo agregemos asi estara ordenado
 
-# agregar un elemento
-planetas.add('Tierra') # con el metodo anidado de los set podemos invocarlo para que nos agrege el elemento y como parametro pasamos el valor a agregar y como en el set no incluye un orden entonces aun agregando este elemento al mostrarlo nos mostrara su orden aleatoriamente
-print(planetas)
+#acceder a un elemento tenemos que proporcionar la llave(key)
+print(diccionario['IDE']) # para acceder al valor de un diccionario tenemos que colocar entre corchetes la llave que contanega el valor que queremos para que nos de su valor
 
-#no se puede duplicar elementos
-planetas.add('Tierra') # como los set no permite duplicar elementos lo que hace python es intetar agregarlo pero como ya existe no lo agrega
-print(planetas)
+# hay otra forma de acceder a una llave y es usando el metodo anidado get y pasando como parametro la key asociado al valor que queremos mostrar
+print(diccionario.get('OOP'))
 
-# eliminar elemento
-planetas.remove('Tierra') # si necesitamos remover un elemento podemos usar el remove como en las listas este metodo lo que hace es buscar el elemento y si es similar al parametro que pasamos entonces remueve el elemento si no no lo hara
-print(planetas)
-
-# si el metodo remove no encuentra el valor nos enviara un error
-# eliminar elemento posiblemente arrojando un error
-#planetas.remove('Tierra')
-#print(planetas)
-
-# hay otra manera de eliminar sin que nos envie un mensaje de error si no encuentra el elemento y es usando el metodo discard funciona igual que remove pero si no encuentra el valor no nos arrojara un error y no eliminara nada si no encuentra el valor
-# eliminar elemento sin arrojar error
-planetas.discard('Jupiter')
-print(planetas)
-
-planetas.discard('Jupiter')
-print(planetas)
-
-# como en las listas tambien podemos limpiar por completo un set sin borrarlo de la memoria
-planetas.clear()
-
-# tambien podemos eliminar por completo incloso de la memoria como la tupla y listas
-del planetas
-print(planetas) # nos arroja un error por que ya no podemos acceder al set de planetas ya que se elimino incluso desde la memoria 
+# como las listas tambien podemos modificar los elementos como se hace en una lista
+diccionario['IDE'] = 'integrated development environment'
+print(diccionario)
