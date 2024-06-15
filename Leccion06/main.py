4c684cc6c817b615df2d06a30cc4891fe1473d9b
Ecos1213 <diasflac1213@gmail.com>
2024-06-14 20:00:28 -0500

    Agrega leccion de argumento en funciones, hay que entender que una cosa es parametros y otro argumento, los parametros son variables definidas en la funcion ( en algunos lenguajes incluso tendrian hasta tipo de dato ademas de las variables) y el argumento son valor3es que pasan cuando llamamos a la funcion, esta pueden ser variables que anteriormente ya han sido definidas con su respectivo tipo de dato (aunque en python esto no aplica) o simplemente colocar el valor directamente en la llamada de la funcion, en python como cualquier lenguaje de progrmacion los parametros simplemente lo agregamos en los parentesis de la funcion definida y los argumentos seria igual, se agregan dentro de los parentesis pero con variables definidas o valores directamente, estos parametros podemos usarlo dentro de la funcion y esto nos ayuda a que podemos reutilizar el llamado de la funcion para ejecutar varias veces la linea de codigo ya sea con el mismo resultado o diferente (ya que podemos pasar juan, perez o karla, lara y imprimir los valores mostrandonos diferentes nombres y apellidos)

--- Leccion06/main.py
+++ Leccion06/main.py
@@ -1,12 +1,9 @@
-#miFuncion() # esto nos aparecera un erorr de miFuncion no esta definida por que primero definimos y despues hacemos la llamada
-# las funcioners en python al principio tenemos que colocar la palabra def y despues el nombre de la funcion que queremos colocarle, podemos usar camelCase o serpent case (serpent case es que cada palabra se separa con un _ ejemplo:funcion_pyhthon)
-def miFuncion(): # despues de usar def para crear una funcion y el nombre de la funcion tenemos que colocar tambien parentesis y en estos parentesis podemos asignar parametros o argumentos y al final usamos los : como lo haciamos con el if, else, elif o ciclos
-    print('Saludos desde mi funcion') # recuerda que todas las lineas que esten identada estaran en nivel de la funcion hasta que una de estas lineas deje de estarlo, ahi se acabara la identacion
+def miFuncion(nombre, apellido): # hay diferencia entre argumento y parametros los parametros son variables definidas en la funcion y el argumento son valores que pasan cuando llamamos a la funcion
+    print('Saludos desde mi funcion')
+    print(f'Nombre: {nombre}, Apellido: {apellido}') # podemos usar los parametros dentro de nuestra funcion
 
-miFuncion() # para que se ejecute la funcion con sus respectivos codigo o bloques de codigo necesitamos llamar la funcion asi como todos los lenguajes de programacion
-# la ventaja de una funcion es poder reutilizarla las veces que sea necesario
-miFuncion()
-miFuncion()
+miFuncion('Juan', 'Perez') # aca pasamos los argumentos que ya son los valores ( ya sea que estos valores esten en una variable o pasar los datos fijamente), si no pasamos estos argumentos nos dara un error por que son obligatorios no opcionales
+miFuncion('Karla', 'Lara')
 
-# pero recuerda esto no es como javascript ya que en python no podemos llamar la funcion y despues definirla, primero tenemos que definirla despues de su definicion podemos escribir en la siguiente lineas de codigo el llamado de esta, si la llamamos antes de su definicion nos aparecera un error
-# mas adelante veremos los modulos en el cual para poder utilizar una funcion tambien podemos importarla asi que si esta funcion se define en otro archivo, en otro modulo, asi que simplmente con importarla vamos a poder utilizarla, sin embargo primero tendriamos que importar la funcion y despues utilizarla
+
+
