a7cebf849fdc9b4a7f7991a72208290857cf2ed1
Ecos1213 <diasflac1213@gmail.com>
2024-06-19 18:58:01 -0500

    Agrega leccion de arguemntos variables en funciones en python, En python podemos pasar argumentos variables en funciones, esto quiere decir que podemos colocar el *NombreDevariable para decirle que vamos a pasarle cualquier cantidad de argumentos a esta funcion, como desconocemos el numero de argumentos total que queremos pasar a una funcion, para esto podemos usar el *NombreDevariable como argumetno dentro de los parentesis, como no sabemos cuantros elementos vamos a recibir en el argumento, podemos anteponer el asterisco al argumento para que tome todos esos parametros del llamado de la funcion, dentro de nuestro codigo en la funcion el argumento con asterisco se convertira en una tupla, de manera interna python este argumento *nombres lo convertira en una tupla con todos los elementos que pasemos del parametro del llamado de una funcion, recuerda que en el llamado de la funcion todos estos elementos pasaran a ser una tupla en el argumento de la funcion pero recuerda que hay que saparar cada dato con una coma para que entienda que es un elemento de la tupla del argumento de la funcion con asterisco, en los llamados de la funcion podemos colocar parametros variados y aun asi la funcion se ejuctara correctamente, esto es una de las ventajas pero recuerda el arguemnto a ser una tupla no permitira ni modificar, ni eliminar ni agregar nuevos elementos, n python el manual veremos de manera frecuento que el argumento variable es : *args (de arguments) pero este nombre es opcional realmente podemso colocar cualquier nombre

--- Leccion06/main.py
+++ Leccion06/main.py
@@ -1,11 +1,9 @@
-def sumar(a = 0, b = 0): # como vimos en los parametros de funciones si en el llamado de la funcion no colocamos los argumentos nos dara un error para evitar esto podemos asignar un valor dentro de los parametros de la funcion para que cuando no se agrege en los argumentos de la funcion use ñps valores por defecto, para dejar valores por defecto simplemente agregamos los datos con igual en las variables que estan en parentesis de la funcion
-    return a + b
+# En python podemos pasar argumentos variables en funciones, esto quiere decir que podemos colocar el *NombreDevariable para decirle que vamos a pasarle cualquier cantidad de argumentos a esta funcion
+def listarNombres(*nombres): # como desconocemos el numero de argumentos total que queremos pasar a una funcion, para esto podemos usar el *NombreDevariable como argumetno dentro de los parentesis, como no sabemos cuantros elementos vamos a recibir en el argumento, podemos anteponer el asterisco al argumento para que tome todos esos parametros del llamado de la funcion
+    # dentro de nuestro codigo el argumento con asterisco se convertira en una tupla, de manera interna python este argumento *nombres lo convertira en una tupla con todos los elementos que pasemos del parametro del llamado de una funcion
+    for nombre in nombres: # como es una tupla podemos iterar tranquilamente, pero recuerda al ser una tupla es inmutable
+        print(nombre)
 
-resultado = sumar(5, 3)
-print(f'Resultado de la suma: {resultado}')
-print(f'Resultado de la suma: {sumar()}') # como vemos no pasamos argumentos y no nos da error pero nos da la suma esto es gracias a que en la funcion tenemos valores por defecto y por eso da 0 por que usa esos valores por defecto y suma 0 + 0 ( ya que a = 0 y b = 0 en valores por defecto)
-
-# aunque si dejamos encima del llamado de la funcion el mouse nos mostrara una pista de los tipos de datos que tenemos que pasarle a la funcion y el tipo de dato de retorno, pero esto es una pista no es obligatorio incluso al no ser obligatorio podemos pasarle cualquier tipo de dato, tambien podemos colocarle este tipo de sintaxis que vemos en la pista pero aun colocandolo al ser solo una pista de datos podemos enviarle aun asi cualquier tipo de dato tanto en los argumentos como en el retorno
-def sumar2(a: int = 0, b: int = 0) -> int: # como vemos podemos colocar una pista de retorno con el simbolo -> y podemos colocarle cualquiert tipo de dato que queremos que retorne y como vimos anteriormente de las pistas de las variables podemos hacerlo igual en los parametros usando : y cualquier tipo de dato que queremos asignarle perto recuerda esto es una pista por ende python no dara error si pasamos otro tipo de dato, ya que en python los tipos de datos son dinamicos
-    return a + b
-print(sumar2('1','0'))
+listarNombres('Juan', 'Karla', 'Maria', 'Ernesto') # recuerda que todos estos elementos pasaran a ser una tupla en el argumento de la funcion pero recuerda que hay que saparar cada dato con una coma para que entienda que es un elemento de la tupla del argumento de la funcion con asterisco
+listarNombres('Laura', 'Carlos') # como tenemos un argumento variable, puede recibir sin ningun problema diferentes cantidad de elementos y funcionara correctamente esto es una de las ventaja pero recuerda que esto lo convertira a una tupla por ende estos elementos no se podran modificar, eliiminar y agregar
+# en python el manual veremos de manera frecuento que el argumento variable es : *args (de arguments) pero este nombre es opcional realmente podemso colocar cualquier nombre
