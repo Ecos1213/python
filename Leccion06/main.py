d1ff2115adc8e386c871a7a0af878bb85b93b638
Ecos1213 <diasflac1213@gmail.com>
2024-06-23 12:25:20 -0500

    python es bastante flexible pero aun asi tenemos que tener cuidado con que informacion estamos proporcionando por que si vemos estamos usando un for(foreach) que recorre un array o tupla o diciconario por ende no podemos pasar solo un numero o booleano por que dara error y tampoco podemos pasar varios parametros por que no es un argumento variable y si solo pasamos un string recorrera cada letra e imprimira cada letra por eso hay que tener cuidado e incluso tener cuidado de como vamos a tratar el dato dentro de la funcion, si queremos pasar o crear una tupla de un solo dato tenemos que colocar entre parentesis el dato y al final del dato una coma, ejemplo: (1,) esto creara una tupla de un solo dato y si queremos pasarlo a un llamado de la funcion seria asi: desplegarNombres((10,))

--- Leccion06/main.py
+++ Leccion06/main.py
@@ -1,14 +1,14 @@
-# en python en los argumentos variables tambien podemos de en vez de pasar una tupla podemos pasar un argumento variable tipo diccionario
-def listarTerminos(**terminos): # con dos asteriscos hace referencia de que ese argumento es un un argumento variable de tipo diccionario, en python en su manual el argumento se llama kwargs hace referencia a un argumento variable de tipo diccionario y es de buena pratica llamar a los argumetnos tipo diccionarios kwargs pero igual se puede colocar cualquier tipo de palabra
-    for llave, valor in terminos.items():
-        print(f'{llave}: {valor}')
+def desplegarNombres(nombres):
+    for nombre in nombres:
+        print(nombre)
 
-listarTerminos() # una cosa a tener en cuenta es que tanto los *args y los **args cuando llamamos el metodo podemos dejarlo vacio por que se puede pasar todos los terminos que queramos o dejarlo vacio y no sucedera ningun error asi sea que lo dejemos vacio
-listarTerminos(IDE='Integrated Development Environment') # como vemos la llave no lleva comillas por eso IDE no tene comillas y el valor puede ser cualquier tipo de dato, para pasar el diccionario primero necesitamos colcoar la key despues el igual = y por ultimo el valor dentro del parentesis del llamado de la funcion, si queremos passar mas argumentos simplemente colcoamos , y dejamos la misma secuencia explicada
-listarTerminos(IDE='Integrated Development Environment', PK='Primary Key')
-listarTerminos(DBMS='Database Management System')
+nombres = ['Juan', 'Karla', 'Guillermo']
 
-def listarTerminos2(nombres, *args, **terminos): # tambien podemos colocar argumentos normales con argumentos varibles tipo tupla y argumentos variables tipo diccionario y podemos usarlo pero esto lo veremos mas detalladamente mas adelante
-    for llave, valor in terminos.items():
-        print(f'{llave}: {valor}')
-
+desplegarNombres(nombres)
+desplegarNombres('Carlos')
+desplegarNombres((10, 11))
+desplegarNombres((10,)) # si queremos pasar o crear una tupla de un solo dato tenemos que colocar entre parentesis el dato y al final del dato una coma
+desplegarNombres([10, 11])
+desplegarNombres(10)
+desplegarNombres(10, 11)
+# python es bastante flexible pero aun asi tenemos que tener cuidado con que informacion estamos proporcionando por que si vemos estamos usando un for(foreach) que recorre un array o tupla o diciconario por ende no podemos pasar solo un numero o booleano por que dara error y tampoco podemos pasar varios parametros por que no es un argumento variable y si solo pasamos un string recorrera cada letra e imprimira cada letra por eso hay que tener cuidado e incluso tener cuidado de como vamos a tratar el dato dentro de la funcion
