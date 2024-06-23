2e1136139100f106e51bebda298f5a3906abc8db
Ecos1213 <diasflac1213@gmail.com>
2024-06-23 17:29:19 -0500

    una funcion recursiva es una funcion que se llama asi misma para completar una cierta tarea como por ejemplo, el ejemplo clasico factorial de un numero, en las funciones internas se ejecutaran una a una hasta tener una respuesta cada uno de ellas y ademas lo que hara python con la funcion interna como no sabe la respuesta al principio quedara en espera y cuando termine ejecutara su operacion, el modo debug podemos ver al lado izquierda en el MainThread cada espera de estas funciones

--- Leccion06/main.py
+++ Leccion06/main.py
@@ -1,14 +1,19 @@
-def desplegarNombres(nombres):
-    for nombre in nombres:
-        print(nombre)
+# una funcion recursiva es una funcion que se llama asi misma para completar una cierta tarea como por ejemplo, el ejemplo clasico factorial de un numero
+"""
+5! = 5 * 4 * 3 * 2 * 1
+5! = 5 * 4 * 3 * 2
+5! = 5 * 4 * 6
+5! = 5 * 24
+5! = 120
 
-nombres = ['Juan', 'Karla', 'Guillermo']
+"""
+def factorial(numero):
+    if numero == 1:
+        return 1
+    else:
+        return numero * factorial(numero-1) # como vemos podemos utilizar la misma funcion dentro de ella y esto lo hacemos para hacer el factorial multiplicando el numero por el factorial del numero - 1 y esto se ejecutara hasta que el valor de numero sea 1 osea que tomara lo siguiente: si colocamos 5 entonces sera 5 * (5-1)(ejecuta esta funcion) 4 * (4-1)(ejecuta esta funcion) 3 * (3-1)(ejecuta esta funcion) 2 * (2-1)(ejecuta esta funcion) 1 como es uno retornara 1 y al ejecutar cada funcion queda asi: 5 * 4 * 3 * 2 * 1 como un factorial
+        # lo que hara python con la funcion interna como no sabe la respuesta al principio quedara en espera y cuando termine ejecutara su operacion, el modo debug podemos ver al lado izquierda en el MainThread cada espera de estas funciones
 
-desplegarNombres(nombres)
-desplegarNombres('Carlos')
-desplegarNombres((10, 11))
-desplegarNombres((10,)) # si queremos pasar o crear una tupla de un solo dato tenemos que colocar entre parentesis el dato y al final del dato una coma
-desplegarNombres([10, 11])
-desplegarNombres(10)
-desplegarNombres(10, 11)
-# python es bastante flexible pero aun asi tenemos que tener cuidado con que informacion estamos proporcionando por que si vemos estamos usando un for(foreach) que recorre un array o tupla o diciconario por ende no podemos pasar solo un numero o booleano por que dara error y tampoco podemos pasar varios parametros por que no es un argumento variable y si solo pasamos un string recorrera cada letra e imprimira cada letra por eso hay que tener cuidado e incluso tener cuidado de como vamos a tratar el dato dentro de la funcion
+numero = 5
+resultado = factorial(numero)
+print(f'El factorial de {numero} es {resultado}')
