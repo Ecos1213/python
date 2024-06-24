7a66ebebeae0b044ab9b7a6e7a41e30a361c7a5b
Ecos1213 <diasflac1213@gmail.com>
2024-06-24 18:00:41 -0500

    Agrega ejercicio resuelto de funcion recursiva, usa una funcion recursiva donde dentro de la misma funcion hacemos el llamado para hacer un contador descendente, una cosa que vimos es que podemos dejar el return vacio osea que no retorne nada, esto lo podemos hacer en python simplemnte dejando solo el return

--- Leccion06/main.py
+++ Leccion06/main.py
@@ -1,19 +1,28 @@
-# una funcion recursiva es una funcion que se llama asi misma para completar una cierta tarea como por ejemplo, el ejemplo clasico factorial de un numero
 """
-5! = 5 * 4 * 3 * 2 * 1
-5! = 5 * 4 * 3 * 2
-5! = 5 * 4 * 6
-5! = 5 * 24
-5! = 120
+Imprimir numeros de 5 a 1 de manera descendete usando funciones recursivas.
+Puede ser cualquier valor positivo, ejemplo, si pasamos el valor de 5, debe imprimir:
+5
+4
+3
+2
+1
+en caso de pasar el valor de 3, debe imprimir:
+3
+2
+1
 
+si se pasan valores negativos no imprime nada
 """
-def factorial(numero):
-    if numero == 1:
-        return 1
-    else:
-        return numero * factorial(numero-1) # como vemos podemos utilizar la misma funcion dentro de ella y esto lo hacemos para hacer el factorial multiplicando el numero por el factorial del numero - 1 y esto se ejecutara hasta que el valor de numero sea 1 osea que tomara lo siguiente: si colocamos 5 entonces sera 5 * (5-1)(ejecuta esta funcion) 4 * (4-1)(ejecuta esta funcion) 3 * (3-1)(ejecuta esta funcion) 2 * (2-1)(ejecuta esta funcion) 1 como es uno retornara 1 y al ejecutar cada funcion queda asi: 5 * 4 * 3 * 2 * 1 como un factorial
-        # lo que hara python con la funcion interna como no sabe la respuesta al principio quedara en espera y cuando termine ejecutara su operacion, el modo debug podemos ver al lado izquierda en el MainThread cada espera de estas funciones
 
-numero = 5
-resultado = factorial(numero)
-print(f'El factorial de {numero} es {resultado}')
+def imprimirNumeroRecursivo(numero):
+    if numero >= 1:
+        print(numero)
+        return imprimirNumeroRecursivo(numero - 1)
+    if numero == 0:
+        return # en python podemos dejar un return vacio y no retornara nada y termina la funcion pero en este caso al ser una funcion recursiva termina la funcion para que las otras se ejecuten hasta terminar de ejecutar todas
+    elif numero <= 0:
+        print('Valor incorrecto...')
+
+imprimirNumeroRecursivo(5)
+imprimirNumeroRecursivo(3)
+imprimirNumeroRecursivo(-3)
