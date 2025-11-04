37f48218f5623292e14b258144c7f949db2de6e6
Ecos1213 <diasflac1213@gmail.com>
2025-11-04 13:38:49 -0500

    Agrega clase multiple variables

---
+++ Platzi/asignacion_multiple.py
@@ -0,0 +1,19 @@
+# Esto es asignacion multiple la primera variable toma el primer valor y asi
+# sucesivamente
+x, y, z = "Manzana", "Naranja", "Banana"
+
+print(x, y, z)  # Asi imprimimos al mismo tiempo varias variables
+
+# De esta manera las 3 variables se asigna con el valor Mandarina
+a = b = c = "Mandarina"
+
+print(a, b, c)
+
+# Cuando son 2 strings podemos concatenar con +
+print("Mi fruta favorita es " + x)
+print(a + " " + z)
+
+# En caso de numero hara sumatorias
+d = 5
+e = 6
+print(d+e)
