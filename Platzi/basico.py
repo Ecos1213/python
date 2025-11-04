b737f54d2bd228a1467dfcecadaee89b0f0feb16
Ecos1213 <diasflac1213@gmail.com>
2025-11-04 11:41:41 -0500

    Agrega clase de sintaxis y modifica commit de basic hola mundo

---
+++ Platzi/basico.py
@@ -0,0 +1,2 @@
+# desde la termina puedes escribir python y ahi puedes escribir codigo
+print("hola mundo")
---
+++ Platzi/sintaxis.py
@@ -0,0 +1,7 @@
+if 5 > 3:  # if tiene que terminar en :
+    # recuerda que en python hay que usar espacios para que entienda que esta
+    # identado en este caso el if, se recomienda 1 sola tabulacion por
+    # sentencia como if y otras
+    print("5 es mayor que 3")
+    if 5 > 1:
+        print("5 es mayor que 1")
