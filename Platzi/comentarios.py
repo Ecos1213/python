d4b564a3354ac5510f6fa021d21f859f2914749e
Ecos1213 <diasflac1213@gmail.com>
2025-11-04 11:47:16 -0500

    Agrega clase de comentarios

---
+++ Platzi/comentarios.py
@@ -0,0 +1,16 @@
+# Con numeral se hace comentarios en python
+print("Hola mundo")  # Imprimir hola mundo
+
+# Este
+# es
+# un
+# comentario
+
+"""
+Comentario de multiple lineas
+este sera
+un comentario
+multilinea
+"""
+
+# print("Esta linea deseo ignorarla")
