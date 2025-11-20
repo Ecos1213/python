72ff22393ae93a78e929ad127b619809b0eaeea3
Ecos1213 <diasflac1213@gmail.com>
2025-11-20 14:25:52 -0500

    Agrega clase de operadores aritmeticos y su precedencia de los operadores

---
+++ Platzi/operadores.py
@@ -0,0 +1,42 @@
+# Operadores aritmeticos
+
+x = 5
+y = 12
+
+# Suma +
+print("Suma", x + y)
+
+# Resta -
+print("Resta", x - y)
+
+# Multiplicacion *
+print("Mutliplicacion", x * y)
+
+# Division / (La division siempre nos da un flotante)
+print("Division", y / x)
+
+# Modulo %
+print("Modulo(resto)", y % x)
+
+# Potencia **
+print("Potencia", y ** x)  # 10 * 10 * 10 * 10 * 10
+
+# Division Entera, no da valor decimal
+print("Division Entera", y // x)
+
+par = 8
+print(par % 2 == 0)
+
+# Precedecia de los operadores
+# osea orden en que se raliza la operacion
+
+# Parentesis: todo lo que este en parentesis va a ser ejecutadio primero
+# Exponentes: Luego sigue los exponentes
+# Mutlicaciones: despues de los exponentes las mutiplicaciones son ejecutadas
+# Divisiones : despues de las mutliplcaiciones sigue las divisones
+# Divisiones Enteras: Luego las divisiones enteras
+# Sumas y restas : Despues las sumas y restas
+# Comparaciones de identidad y pertenencia: vienen despues las comparaciones de identidad y pertenencia
+# Operadores logicos: Y despues los operadores logicos
+
+# y todo este es el orden ejecutado por python secuencialmente
