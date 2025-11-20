61de2584a452fabac04599624a4d2cbd05906722
Ecos1213 <diasflac1213@gmail.com>
2025-11-20 12:50:47 -0500

    Agrega clase de tipo none

---
+++ Platzi/none.py
@@ -0,0 +1,11 @@
+# El tipo de dato none es la ausencia del valor, no es un string vacio, o el
+# numero 0 o False
+x = None
+print(x)  # dara none
+print(type(x))  # dara NoneType
+print(type(""))  # da string vacio
+print(type(0))  # da 0
+print(type(False))  # da False
+print(type([]))  # da un array vacio
+
+# por eso podemos decir que none es la ausencia de valor es diferente a cualquier tipo es unico
